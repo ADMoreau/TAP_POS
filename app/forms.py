17c502b1cf88259b504684f57d0e1a4f20e9ccf8
ADMoreau <amoreau876@gmail.com>
2019-11-03 22:07:46 -0600

    dropdown menus and db functionality

--- app/forms.py
+++ app/forms.py
@@ -1,69 +1,8 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileAllowed, FileRequired
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, Field, Form, RadioField, SelectField, \
-    FileField, FormField
+from wtforms import StringField, SubmitField, RadioField, SelectField
 from wtforms.validators import DataRequired
 
-from app.db_handler import get_names
-
-
 patterns = ['bpm', 'radialpatternshift', 'juggle']
-
-class SetBeer(FlaskForm):
-    '''
-    form used for home page to create the drop down menus to select beers
-    '''
-    BEER_CHOICES = get_names()
-    tap1 = SelectField(label='Tap 1', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap2 = SelectField(label='Tap 2', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap3 = SelectField(label='Tap 3', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap4 = SelectField(label='Tap 4', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap5 = SelectField(label='Tap 5', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap6 = SelectField(label='Tap 6', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap7 = SelectField(label='Tap 7', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap8 = SelectField(label='Tap 8', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap9 = SelectField(label='Tap 9', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap10 = SelectField(label='Tap 10', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap11 = SelectField(label='Tap 11', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap12 = SelectField(label='Tap 12', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap13 = SelectField(label='Tap 13', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap14 = SelectField(label='Tap 14', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap15 = SelectField(label='Tap 15', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap16 = SelectField(label='Tap 16', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap17 = SelectField(label='Tap 17', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap18 = SelectField(label='Tap 18', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap19 = SelectField(label='Tap 19', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap20 = SelectField(label='Tap 20', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap21 = SelectField(label='Tap 21', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap22 = SelectField(label='Tap 22', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap23 = SelectField(label='Tap 23', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap24 = SelectField(label='Tap 24', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap25 = SelectField(label='Tap 25', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap26 = SelectField(label='Tap 26', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap27 = SelectField(label='Tap 27', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap28 = SelectField(label='Tap 28', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap29 = SelectField(label='Tap 29', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap30 = SelectField(label='Tap 30', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap31 = SelectField(label='Tap 31', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap32 = SelectField(label='Tap 32', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap33 = SelectField(label='Tap 33', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap34 = SelectField(label='Tap 34', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap35 = SelectField(label='Tap 35', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap36 = SelectField(label='Tap 36', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap37 = SelectField(label='Tap 37', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap38 = SelectField(label='Tap 38', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap39 = SelectField(label='Tap 39', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    tap40 = SelectField(label='Tap 40', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-
-
-
-class SelectEdit(FlaskForm):
-    '''
-    used to edit previously saved beers
-    '''
-    BEER_CHOICES = get_names()
-    beer = SelectField(label='Beer', choices=[(str(beer), str(beer)) for beer in BEER_CHOICES])
-    submit = SubmitField('Submit')
 
 
 class BeerForm(FlaskForm):
---
+++ app/models.py
@@ -0,0 +1,83 @@
+from flask import request
+from sqlalchemy import MetaData, create_engine, Table, Column, select, ForeignKey, Integer, String, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+
+
+metadata = MetaData()
+engine = create_engine('sqlite:///beers.db', connect_args={'check_same_thread': False}, echo=False)  # echo=False
+Base = declarative_base()
+db_session = sessionmaker(bind=engine)()
+
+
+class Beer(Base):
+    __tablename__ = 'beers'
+
+    name = Column(String(64), primary_key=True, nullable=False)
+    val1 = Column(Integer, nullable=False)
+    val2 = Column(Integer, nullable=False)
+    val3 = Column(Integer, nullable=False)
+    val4 = Column(Integer, nullable=False)
+    val5 = Column(Integer, nullable=False)
+    rarity = Column(Integer, nullable=False)
+    abv = Column(Integer, nullable=False)
+    pattern = Column(Integer, nullable=False)
+    tap = Column(Integer, nullable=False)
+
+    def __rep__(self):
+        return '<BEER {}>'.format(self.name)
+
+
+def delete_beer(beer):
+    db_session.query(Beer).filter_by(name=beer.name).delete()
+    db_session.commit()
+
+
+def get_records():
+    return db_session.query(Beer).all()
+
+
+def get_beer_by_name(beername):
+    beer = db_session.query(Beer).filter_by(name=beername).first()
+    return beer
+
+
+def get_names():
+    beers = db_session.query(Beer).all()
+    return [beer.name for beer in beers]
+
+
+def insert(form):
+    try:
+        beer = Beer()
+        beer.val1 = int(request.form['val1'])
+        beer.val2 = int(request.form['val2'])
+        beer.val3 = int(request.form['val3'])
+        beer.val4 = int(request.form['val4'])
+        beer.val5 = int(request.form['val5'])
+        beer.name = str(request.form['beername'])
+        beer.rarity = int(request.form['rarity'])
+        beer.abv = int(request.form['abv'])
+        beer.pattern = int(request.form['pattern'])
+        beer.tap = -1
+        db_session.add(beer)
+        db_session.commit()
+        return "Beer created"
+    except Exception as e:
+        return e
+
+
+'''
+def set_tap(tap, beer_name):
+    try:
+        task = (tap, beer_name)
+        sql = ''''''
+            UPDATE beers
+            SET tap = ?
+            WHERE name = ?
+            ''''''
+        commit(sql, task)
+        return "tap {} set to {}".format(tap, beer_name)
+    except Exception as e:
+        return e
+'''
